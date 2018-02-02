8b09a6f8b06a3fbfea1080ed23d2cd52e9f999dc
Ellie Birbeck <elliebirbeck@hotmail.co.uk>
2018-02-02 18:16:00 +0000

    console param input

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import requests
 import config
+import sys
 from bs4 import BeautifulSoup
 
 
-def get_artist_id(artist_name="Kanye West"):
+def get_artist_id(artist_name):
 
 	artist_id = None
 	params = {'q': artist_name}
@@ -15,9 +16,12 @@ def get_artist_id(artist_name="Kanye West"):
 			artist_id = hit["result"]["primary_artist"]["id"]
 			break
 
-	print("Collecting songs for", artist_name, ": Artist ID", artist_id)
-
-	return artist_id
+	if artist_id:
+		print("Collecting songs for", artist_name, ": Artist ID", artist_id)
+		return artist_id
+	else:
+		print("Artist", artist_name, "not found.")
+		sys.exit(0)
 
 
 def get_song_urls(artist_id):
@@ -75,6 +79,13 @@ def get_lyrics(song_urls):
 
 if __name__ == '__main__':
 
+	# artist input
+	if len(sys.argv[1:])==0:
+		print("You must pass a parameter for artist name e.g. 'Kanye West'")
+		sys.exit(0)
+	else:
+		artist_name = sys.argv[1]
+
 	# api key and authorisation
 	base_url = "http://api.genius.com"
 	api_key = config.access_token
@@ -82,15 +93,16 @@ if __name__ == '__main__':
 	headers = {'Authorization': auth_string}
 
 	# get lyrics
-	artist_id = get_artist_id()
-	if not artist_id:
-		print("Artist not found.")
-		# quit
+	artist_id = get_artist_id(artist_name)
 	song_urls = get_song_urls(artist_id)
 	lyrics = get_lyrics(song_urls)
 
 	# save to txt file
-	f = open("lyrics.txt","w")
+	if sys.argv[2]:
+		filename = sys.argv[2] + ".txt"
+	else:
+		filename = "lyrics.txt"
+	f = open(filename,"w")
 	for l in lyrics:
 		f.write(l)
 	f.close()
